29f8eaf5b4aa3d3f7bb73c64e3797c5d130db5cf
Rian Aquino <rian.aquino@outlook.com>
2023-08-21 10:04:59 -0300

    PDM - TP01

---
+++ PDM - Programação para Dispositivos Móveis/TP01/TP01/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿/* 
+ Dupla:
+ Gabriel Ferreira de Souza
+ Rian Gustavo Quintanilha de Aquino
+ */
+namespace TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSubmit(object sender, EventArgs e)
+        {
+
+            if (id.Text == "admin" && password.Text == "senha@dmin")
+                DisplayAlert("Logado com sucesso!", "", "ok");
+            else
+                DisplayAlert("Não autorizado!", "Você quis dizer?\n\nid: admin\npass: senha@dmin", "ok");
+        }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            id.Text = "";
+            password.Text = "";
+        }
+
+        private void OnCredits(object sender, EventArgs e) 
+        {
+            DisplayAlert("Realizado por", "Gabriel Ferreira de Souza\nRian Gustavo Quintanilha de Aquino", "Nota 10");
+        }
+    }
+}
